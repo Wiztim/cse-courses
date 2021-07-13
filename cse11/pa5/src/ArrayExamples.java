1a9da656f9cf967c24244adfc99587aefa9b7e31
Wiztim <timothyorlov@gmail.com>
2021-07-12 23:18:10 -0700

    skeleton file for ArrayExamples (#8)

---
+++ cse11/pa5/src/ArrayExamples.java
@@ -0,0 +1,40 @@
+public class ArrayExamples {
+    // returns a string with the array contents separated by string
+    String joinWith(String[] stringArray, String separator) {
+        return "";
+    }
+
+    // returns true if all booleans are true
+    boolean allTrue(boolean[] booleanArray) {
+        return true;
+    }
+
+    // returns true if all numbers in array are within low and high (inclusive)
+    boolean allWithinRange(double[] numArray, double low, double high) {
+        return true;
+    }
+
+    // creates a pair object with the lowest and highest
+    Pair maxim(int[] numArray) {
+        return new Pair(1,1);
+    }
+
+    // returns the alphabetically first string
+    String earliest(String[] stringArray) {
+        return "";
+    }
+
+    int lookup(String[] keys, int[] values, String key) {
+        return -1;
+    }
+}
+
+class Pair {
+    int a;
+    int b;
+
+    Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
