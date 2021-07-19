c90c211c954d2761e7434f9c45e7d2a2c6d44bfa
Wiztim <timothyorlov@gmail.com>
2021-07-19 13:32:15 -0700

    added skeleton of StringSeach and file reading (#14)

---
+++ cse11/pa7/src/Transformations.java
@@ -0,0 +1,36 @@
+public class Transformations {
+    // modifies the lines to be all upper case
+    String[] upperCase(String[] fileText) {
+        String[] modifiedString = new String[fileText.length];
+
+        return modifiedString;
+    }
+
+    // modifies the lines to be all lower case
+    String[] lowerCase(String[] fileText) {
+        String[] modifiedString = new String[fileText.length];
+
+        return modifiedString;
+    }
+
+    // keeps on the first int amount of characters
+    String[] takeFirst(String[] fileText, int position) {
+        String[] modifiedString = new String[fileText.length];
+
+        return modifiedString;
+    }
+
+    // keeps only the last int amount of characters
+    String[] takeLast(String[] fileText, int position) {
+        String[] modifiedString = new String[fileText.length];
+
+        return modifiedString;
+    }
+
+    // replaces all instances of the first string with the second string
+    String[] replaceInstance(String[] fileText, String toReplace, String replacement) {
+        String[] modifiedString = new String[fileText.length];
+
+        return modifiedString;
+    }
+}
